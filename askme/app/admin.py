6e3013c6ed67a75e30e13a383f2324cbd4821645
danxxo <nikitka.sarkisyan@mail.ru>
2023-11-01 23:17:59 +0300

    add models

--- askme/app/admin.py
+++ askme/app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Tag, Question, Profile, Answer
 
-# Register your models here.
+
+admin.site.register(Tag)
+admin.site.register(Question)
+admin.site.register(Answer)
+admin.site.register(Profile)
+
+
--- askme/app/models.py
+++ askme/app/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    profile = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    userAvatar = models.ImageField(default='upload/default/user.png')
+
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Question(models.Model):
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    content = models.TextField(null=True, blank=True)
+    likes = models.ManyToManyField(Profile, 
+                                   related_name='questions', 
+                                   blank=True)
+    tags = models.ManyToManyField(Tag, related_name='questions', blank=True)
+
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    content = models.TextField()
+    likes = models.ManyToManyField(Profile,
+                                   related_name='answers',
+                                   blank=True)
+    isCorrect = models.BooleanField(default=False)

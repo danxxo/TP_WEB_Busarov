8d9a2ae1d6923883719bba7983a41c916b1f8c13
danxxo <nikitka.sarkisyan@mail.ru>
2023-11-08 21:05:05 +0300

    add pagination

--- askme/app/views.py
+++ askme/app/views.py
@@ -1,5 +1,9 @@
+import imp
 from random import randrange
+import re
+from tkinter import N
 from django.shortcuts import render
+from django.core.paginator import Paginator
 
 
 QUESTIONS = [
@@ -9,13 +13,32 @@ QUESTIONS = [
         'likes': randrange(1000),
         'content': f'Question {i} content. A lot of text, many many unknown info',
         'tags': [f'Tag1-{j}' for j in range(i, i+5)]
-    } for i in range(10)
+    } for i in range(1000)
 ]
+
+def paginate(objects, request):
+    page = request.GET.get('page')
+    if page is None:
+        page=1
+    try:
+        print(int(15.2))
+        page = int(page)
+    except ValueError:
+        page = 1
+
+    paginator = Paginator(objects, per_page=5)
+    if page > paginator.num_pages:
+        page = paginator.num_pages
+
+    pg = paginator.page(page)
+    print(pg.object_list)
+
+    return paginator.page(page)
 
 def index(request):
     return render(request,
                   'app/index.html',
-                  {'questions': QUESTIONS})
+                  {'questions': paginate(QUESTIONS, request)})
 
 def question_detail(request, question_id):
     question = QUESTIONS[question_id]
